7486e9e48c1d4ece4f6b3f5d25708d8af4ad3eb8
Menersar <48289383+Menersar@users.noreply.github.com>
2023-07-09 15:39:55 +0200

    Update main-hmr.ts

--- packages/electron-webpack/src/electron-main-hmr/main-hmr.ts
+++ packages/electron-webpack/src/electron-main-hmr/main-hmr.ts
@@ -7,7 +7,7 @@ if (socketPath == null) {
 
 // module, but not relative path must be used (because this file is used as entry)
 // const HmrClient = require("electron-webpack/out/electron-main-hmr/HmrClient").HmrClient
-const HmrClient = require("git@github.com:Menersar/electron-webpack/packages/electron-webpack/out/electron-main-hmr/HmrClient.git").HmrClient
+const HmrClient = require("electron-webpack/out/electron-main-hmr/HmrClient").HmrClient
 // tslint:disable:no-unused-expression
 new HmrClient(socketPath, (module as any).hot, () => {
   return __webpack_hash__
